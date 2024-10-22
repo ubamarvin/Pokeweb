331d391cae2521fb0620b8bc22afe15470293b63
MarvinUba <ubamarvin95@gmail.com>
2024-10-22 17:53:30 +0200

    this fetches the game.json file from the server

---
+++ public/javascripts/jsonFetcher.js
@@ -0,0 +1,32 @@
+class JsonFetcher {
+    constructor() {
+      this.isLoading = false; // Indicator for loading state
+      this.gameData = null; // Store fetched game data
+    }
+  
+    async fetchGameJson() {
+      this.isLoading = true; // Set loading state to true
+      try {
+        const response = await fetch('http://localhost:9000/json'); // Adjust the URL as necessary
+        
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+  
+        this.gameData = await response.json(); // Store the fetched data
+        //console.log(this.gameData); // Use the fetched data here
+      } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+      } finally {
+        this.isLoading = false; // Reset loading state
+      }
+    }
+}
+// for some magical reason parantheses are needed here...
+const jsonFetcher = new JsonFetcher();
+
+(async () => {
+    await jsonFetcher.fetchGameJson();
+})();
+
+export default jsonFetcher;
